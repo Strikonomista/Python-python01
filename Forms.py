0e54ba49b630f8520c54dfe6ab0ad54d7368685f
Otavio <137859594+Strikonomista@users.noreply.github.com>
2025-01-18 01:59:41 -0300

    Add files via upload

---
+++ Forms.py
@@ -0,0 +1,72 @@
+import pandas
+import openpyxl
+import pyautogui
+import time
+
+
+pyautogui.press('win')
+time.sleep(0.4)
+pyautogui.write('Opera')
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write('https://docs.google.com/forms/d/e/1FAIpQLSfZbbrkb2-SlTPcEjwj-uW397rQDEwCFVgW8JEjTiBsXWKMEg/viewform', interval=0.01)
+pyautogui.press('enter')
+
+for _ in range(60):
+    time.sleep(2)
+    pyautogui.press('tab')
+    time.sleep(2)
+    pyautogui.press('tab')
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+        #pagina2
+    time.sleep(1)
+    pyautogui.click(x=691, y=407, clicks=2, interval=0.5, button = 'left')
+    pyautogui.scroll(-1500)
+    pyautogui.click(x=686, y=367, clicks=2, interval =0.5, button = 'left')
+    pyautogui.click(x=682, y=555, clicks=1, interval =0.5, button = 'left')
+    pyautogui.click(x=687, y=826, clicks=1, interval =0.5, button = 'left')
+    pyautogui.click(x=803, y=975, clicks=1, interval =0.5, button = 'left')
+            #  https://imgur.com/gallery/one-more-reason-to-always-carry-knife-PKZa0wv garrafa fanta
+    time.sleep(2)
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    time.sleep(1)
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.click(x=688, y=550, clicks=1, interval =0.5, button = 'left')
+    time.sleep(1)
+    pyautogui.press('tab')  
+    pyautogui.click(x=684, y=774, clicks=1, interval =0.5, button = 'left')
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.click(x=681, y=550, clicks=1, interval =0.5, button = 'left')
+    time.sleep(1)
+    pyautogui.press('tab')
+    pyautogui.write("spamma no bot vai")
+    time.sleep(1)
+    pyautogui.click(x=790, y=844, clicks=1, interval =0.5, button = 'left')
+    time.sleep(1)
+    pyautogui.press('F5') 
+
+
+
+
+
+
+
+
+
